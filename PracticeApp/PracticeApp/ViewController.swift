f4d662722ce34994b581b243db8e629c6b436c54
Ewoud Wortelboer <ewoud@tacx.com>
2020-06-24 11:49:07 +0200

    Initial commit.

---
+++ PracticeApp/PracticeApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PracticeApp
+//
+//  Created by Ewoud Wortelboer on 24/06/2020.
+//  Copyright © 2020 Ewoud Wortelboer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
